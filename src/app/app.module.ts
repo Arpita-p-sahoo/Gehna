8ce77f5480494070a5c5bdfa4701d5d8c143fa4e
Arpita.s <arpita.sahoo637174@gail.com>
2024-06-06 21:38:54 +0530

    Gehna first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { UserComponent } from './user/user.component';
+import { SearchComponent } from './search/search.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    CartComponent,
+    UserComponent,
+    SearchComponent,
+    NavComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
